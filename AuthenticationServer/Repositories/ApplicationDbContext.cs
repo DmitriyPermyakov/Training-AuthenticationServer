cb149fe71afdd5015daa163f7b050b26f91159ae
Dmitriy Permyakov <dmitrypermyakov1990@gmail.com>
2023-02-05 11:41:17 +0300

    Added RoleEnumConverter

---
+++ AuthenticationServer/Repositories/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AuthenticationServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationServer.Repositories
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    }
+}
---
+++ AuthenticationServer/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using AuthenticationServer.Models;
+
+namespace AuthenticationServer.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task CreateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
